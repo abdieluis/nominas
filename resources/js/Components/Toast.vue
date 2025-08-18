83f0a779066500b58383d56a63375321c375ccc4
abdielGraciano <luisabdiel.gv@gmail.com>
2025-08-18 10:52:42 -0600

    mensajes exito y error globales

--- resources/js/Components/Toast.vue
+++ resources/js/Components/Toast.vue
@@ -1,27 +1,8 @@
-<script setup>
-import { reactive, watch } from 'vue';
-import { toastStore } from '@/Stores/toastStore.js';
-
-// Esto es lo que reacciona a los cambios en el store
-const toastClass = reactive({
-    'bg-green-500': false,
-    'bg-red-500': false
-});
-
-
-watch(() => toastStore.type, (newType) => {
-    toastClass['bg-green-500'] = newType === 'success';
-    toastClass['bg-red-500'] = newType === 'error';
-}, { immediate: true });
-
-</script>
 <template>
-    <transition>
-        <div v-if="toastStore.show" class="fixed bottom-5 right-5 z-50 p-4 rounded-md shadow-lg" :class="toastClass">
-            <p class="text-white">{{ toastStore.message }}</p>
-            <Toast />
-            <Button label="Success" severity="success" @click="showSuccess" />
-        </div>
-    </transition>
+    <Toast />
 </template>
 
+<script setup>
+// Importa el componente de Toast de PrimeVue.
+import Toast from 'primevue/toast'; 
+</script>
